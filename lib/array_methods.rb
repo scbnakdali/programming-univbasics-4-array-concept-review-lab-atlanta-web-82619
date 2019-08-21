31c08143c7a1636dbd02f0dc4c6c8f34c0e59f0c
Sarah Bullard Nakdali <scadmusbullard@gmail.com>
2019-08-21 12:59:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,14 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  #Looping through an array to find the position of a value
+  value_to_find
 end
 
 def find_max_value(array)
-  # Add your solution here
-end
+  #Finding a maximum value in an array
+  find_max_value.max(array)
+end 
 
 def find_min_value(array)
-  # Add your solution here
+ #Finding a minimum value in an array
+ [10,5,3,7,19,1,3,10].min 
 end
